0bff1bfab58e17b66bfae5df9bc79fd384d7e083
griffinbaker12 <griffin_baker12@icloud.com>
2022-05-18 01:09:36 -0500

    added new recursion examples

--- Recursion/countdown.js
+++ Recursion/countdown.js
@@ -23,4 +23,23 @@ function maxNumber(arr) {
   return arr[0] > subMax ? arr[0] : subMax;
 }
 
-console.log(maxNumber([2, 7, 9, 1, 3, 5]));
+// console.log(maxNumber([2, 7, 9, 1, 3, 5]));
+
+function binarySearchRecursive(n, arr, low, high) {
+  if (low === undefined || high === undefined) {
+    (low = 0), (high = arr.length - 1);
+  }
+  const guess = Math.floor((high + low) / 2);
+  if (high >= low) {
+    if (n === arr[guess]) return guess;
+    if (n > arr[guess]) {
+      return binarySearchRecursive(n, arr, guess + 1, high);
+    }
+    if (n < arr[guess]) {
+      return binarySearchRecursive(n, arr, 0, guess - 1);
+    }
+  }
+  return 'not in list';
+}
+
+console.log(binarySearchRecursive(4, [5, 1, 3, 9, 6, 10, 15]));
